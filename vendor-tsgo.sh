ddafa008d46e19327ea9d22a943bda5ae10d1403
Martin Najemi <martin@najemi.cz>
2026-02-11 13:55:32 +0100

    chore: Make vendor-tsgo.sh work on mac and linux
    Risk: low
    JIRA: STL-2317

--- vendor-tsgo.sh
+++ vendor-tsgo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Portable in-place sed (macOS needs -i '', Linux needs -i)
+if [[ "$(uname)" == "Darwin" ]]; then
+  SEDI=(sed -i '')
+else
+  SEDI=(sed -i)
+fi
+
 VENDOR_DIR="_vendor/typescript-go"
 
 # Clean previous vendor
@@ -20,19 +27,19 @@ mv "$VENDOR_DIR/internal" "$VENDOR_DIR/pkg"
 
 # Rewrite all import paths: internal/ -> pkg/
 echo "Rewriting import paths..."
-find "$VENDOR_DIR" -name "*.go" -type f -exec sed -i '' \
+find "$VENDOR_DIR" -name "*.go" -type f -exec "${SEDI[@]}" \
   's|github.com/microsoft/typescript-go/internal/|github.com/microsoft/typescript-go/pkg/|g' {} +
 
 # Rewrite the module name in go.mod so we can use a replace directive
 echo "Patching go.mod..."
-sed -i '' 's|^module github.com/microsoft/typescript-go|module goodchanges/tsgo-vendor|' "$VENDOR_DIR/go.mod"
+"${SEDI[@]}" 's|^module github.com/microsoft/typescript-go|module goodchanges/tsgo-vendor|' "$VENDOR_DIR/go.mod"
 
 # Also rewrite self-references if any exist beyond internal/
-find "$VENDOR_DIR" -name "*.go" -type f -exec sed -i '' \
+find "$VENDOR_DIR" -name "*.go" -type f -exec "${SEDI[@]}" \
   's|github.com/microsoft/typescript-go|goodchanges/tsgo-vendor|g' {} +
 
 # Fix go.mod replace path references too
-sed -i '' 's|github.com/microsoft/typescript-go|goodchanges/tsgo-vendor|g' "$VENDOR_DIR/go.mod"
+"${SEDI[@]}" 's|github.com/microsoft/typescript-go|goodchanges/tsgo-vendor|g' "$VENDOR_DIR/go.mod"
 
 echo "Done. Vendored tsgo at $VENDOR_DIR"
 echo "Module: goodchanges/tsgo-vendor"
